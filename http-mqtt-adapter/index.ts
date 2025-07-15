6856cafe22b4769d726d02e1053910f49752f5c9
BrunBrand <brandon.brunno@gmail.com>
2025-07-15 01:11:49 -0300

    implement http-mqtt-adapter

---
+++ http-mqtt-adapter/index.ts
@@ -0,0 +1,42 @@
+const http = require('http');
+import http from "http"
+import mqtt from "mqtt"
+
+const port = 3001;
+const client = mqtt.connect("mqtt:localhost:1883")
+
+const pushTopic = "raw"
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/data') {
+    let body = '';
+    req.on('data', chunk => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      try {
+        const data = JSON.parse(body);
+        console.log('Received data:', data);
+
+        forwardData(data, res);
+        res.writeHead(200, {"Content-Type": "text/plain"})
+        res.end("Data Received")
+      } catch (error) {
+        res.writeHead(400, { 'Content-Type': 'text/plain' });
+        res.end('Invalid JSON payload');
+      }
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+function forwardData(data) {
+  
+  client.publish(pushTopic, JSON.stringify(data))
+}
+
+server.listen(port, () => {
+  console.log(`Data Ingestion Service listening on port ${port}`);
+});
