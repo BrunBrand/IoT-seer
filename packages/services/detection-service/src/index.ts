7ff276330fb262501b34f9a42ee9286795af4752
BrunBrand <brandon.brunno@gmail.com>
2025-07-16 01:42:24 -0300

    Solve configuration dependency - non working commit

--- packages/services/detection-service/src/index.ts
+++ packages/services/detection-service/src/index.ts
@@ -1,36 +1,53 @@
 import mqtt from "mqtt"
 
-const pullTopic="treated"
-const pushTopic="score"
+import {config} from "@iot-seer/config"
+import {z} from "zod"
 
-const broker="mqtt://localhost:1883"
-const client = mqtt.connect(broker)
+const broker_url=config.MQTT_BROKER_URL
+const client = mqtt.connect(broker_url)
+
+const incomingMessageSchema = z.object({"average": z.coerce.number().default(0)})
 
 client.on("connect", ()=>{
-  console.log("Connected to MQTT broker")
-  client.subscribe(pullTopic);
+  console.log(`Connected to MQTT broker ${broker_url}`)
+  client.subscribe(config.TOPICS.PROCESSED_DATA); 
 })
 
 client.on("error", (error)=>{
-  console.error("MQTT connection error:", error)
+  console.error(`MQTT connection ${broker_url} error:`, error)
 })
 
 
-client.on("message", (topic, message)=>{
+client.on("message", (topic:string, message:Buffer)=>{
 
-  console.log(`Received message on topic "${topic}": ${message.toString()}`);
+  if(config.NODE_ENV==="development"){
+    console.log(`Received message on topic "${topic}": ${message.toString()}`);
+  }
   
   try{
-    const data = JSON.parse(message)
-    const anomalyScore = Math.random()
-    forwardData(data.average, anomalyScore) 
+    const data = JSON.parse(message.toString())
+    const dataValidated = messageValidator(data)
+    const anomalyScore = getAnomalyScore()
+    forwardData(dataValidated.average, anomalyScore) 
   }catch(error){
-    console.error("Error parsing JSON:", error)
-    return
+    if(error instanceof SyntaxError){
+      console.error("Error parsing JSON:", error)
+    } else if(error instanceof z.ZodError){
+      console.error("Validation error:", error)
+    } else {
+      console.error("Unexpected error:",error)
+    }
   }
-
 })
 
-function forwardData(average, anomalyScore) {
-  client.publish(pushTopic, JSON.stringify({average, anomalyScore}))
+function messageValidator(message: Object){
+  return incomingMessageSchema.parse(message)
+}
+
+function forwardData(average:number, anomalyScore:number) {
+  client.publish(config.TOPICS.ANALYSIS_RESULTS, JSON.stringify({average, anomalyScore}))
+}
+
+function getAnomalyScore(): number{
+  return Math.random()
 }
